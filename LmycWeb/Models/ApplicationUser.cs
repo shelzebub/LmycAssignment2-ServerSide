461163ab86381d78c478d127d0f8311743ff42c7
shelzebub <makaxsole@gmail.com>
2018-04-04 20:49:32 -0700

    Added Boat model, reinitialized login and register settings

--- LmycWeb/Models/ApplicationUser.cs
+++ LmycWeb/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LmycWeb.Models.LmycBoatClub;
 using Microsoft.AspNetCore.Identity;
 
 namespace LmycWeb.Models
@@ -9,5 +10,16 @@ namespace LmycWeb.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string MobileNumber { get; set; }
+        public double SailingExperience { get; set; }
+
+        public List<Boat> Boats { get; set; }
     }
 }
