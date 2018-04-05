edff1eaa9bcdd28dc3820588ad76276c2fe6efc0
shelzebub <makaxsole@gmail.com>
2018-04-04 21:34:43 -0700

    create dbInitializer for seeding users and boats

---
+++ LmycWeb/Data/DbInitializer.cs
@@ -0,0 +1,147 @@
+﻿using LmycWeb.Models;
+using LmycWeb.Models.LmycBoatClub;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LmycWeb.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                var admin = new ApplicationUser
+                {
+                    UserName = "a",
+                    Email = "a@a.a",
+                    FirstName = "Jason",
+                    LastName = "Chen",
+                    StreetAddress = "3700 Willingdon Ave",
+                    City = "Burnaby",
+                    Province = "BC",
+                    PostalCode = "V5G 3H2",
+                    Country = "Canada",
+                    MobileNumber = "(604) 434-5734",
+                    SailingExperience = 50
+                };
+
+                var adminId = await SeedingUser(serviceProvider, admin, "P@$$w0rd");
+
+                var member = new ApplicationUser
+                {
+                    UserName = "m",
+                    Email = "m@m.m",
+                    FirstName = "Siri",
+                    LastName = "Apple",
+                    StreetAddress = "3700 Willingdon Ave",
+                    City = "Burnaby",
+                    Province = "BC",
+                    PostalCode = "V5G 3H2",
+                    Country = "Canada",
+                    MobileNumber = "(604) 434-5734",
+                    SailingExperience = 2
+                };
+
+                var memberId = await SeedingUser(serviceProvider, member, "P@$$w0rd");
+
+            }
+        }
+
+        private static async Task<string> SeedingUser(IServiceProvider serviceProvider, ApplicationUser newUser, string password)
+        {
+            var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+            var user = await userManager.FindByNameAsync(newUser.UserName);
+
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = newUser.UserName,
+                    Email = newUser.Email,
+                    FirstName = newUser.FirstName,
+                    LastName = newUser.LastName,
+                    StreetAddress = newUser.StreetAddress,
+                    City = newUser.City,
+                    Province = newUser.Province,
+                    PostalCode = newUser.PostalCode,
+                    Country = newUser.Country,
+                    MobileNumber = newUser.MobileNumber,
+                    SailingExperience = newUser.SailingExperience
+                };
+
+                await userManager.CreateAsync(user, password);
+            }
+            return user.Id;
+        }
+
+        public static List<Boat> GetBoats(ApplicationDbContext db)
+        {
+            List<Boat> boats = new List<Boat>()
+            {
+                new Boat()
+                {
+                    BoatName = "King George",
+                    Picture = "http://www.lmyc.ca/sites/default/files/content/pages/sharqui%20photo.jpg",
+                    LengthInFeet = 33,
+                    Make = "LMYC",
+                    Year = 2016,
+                    CreationDate = DateTime.Today,
+                    User = db.Users.FirstOrDefault (u => u.UserName == "a" )
+                },
+
+                new Boat()
+                {
+                    BoatName = "QueensBorough",
+                    Picture = "http://www.lmyc.ca/sites/default/files/content/pages/white%20swan.jpg",
+                    LengthInFeet = 50,
+                    Make = "Vancouvoat",
+                    Year = 2018,
+                    CreationDate = DateTime.Today,
+                    User = db.Users.FirstOrDefault (u => u.UserName == "a" )
+                },
+
+                new Boat()
+                {
+                    BoatName = "Otter",
+                    Picture = "http://www.lmyc.ca/sites/default/files/Lightcurefleetpic.jpg",
+                    LengthInFeet = 33,
+                    Make = "The Otter Company",
+                    Year = 2017,
+                    CreationDate = DateTime.Today,
+                    User = db.Users.FirstOrDefault (u => u.UserName == "a" )
+                },
+
+                new Boat()
+                {
+                    BoatName = "WaffleBoat",
+                    Picture = "http://www.lmyc.ca/sites/default/files/content/pages/frankie.jpg",
+                    LengthInFeet = 28,
+                    Make = "Waffle Incorporations",
+                    Year = 2018,
+                    CreationDate = DateTime.Today,
+                    User = db.Users.FirstOrDefault (u => u.UserName == "a" )
+                },
+
+                new Boat()
+                {
+                    BoatName = "KoiSailer",
+                    Picture = "http://www.lmyc.ca/sites/default/files/content/pages/Small%20pegasus.jpg",
+                    LengthInFeet = 22,
+                    Make = "Koi & Co.",
+                    Year = 2018,
+                    CreationDate = DateTime.Today,
+                    User = db.Users.FirstOrDefault (u => u.UserName == "a" )
+                },
+
+            };
+            return boats;
+        }
+    }
+}
